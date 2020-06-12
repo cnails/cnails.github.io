0015ea082bea7f5ec94523afd9f74d8366f136c6
cnails <andreyandreevgr@gmail.com>
2020-06-12 23:33:22 +0300

    a

---
+++ rend.php
@@ -0,0 +1,26 @@
+<?php
+    class Render {
+        private $_html;
+
+        public function __construct($av) {
+            if (!isset($av['html']) || !isset($av['param']) || !file_exists($av['html'])) {
+                return;
+            }
+            $fd = fopen($av['html'], "r");
+            while (!feof($fd)) {
+                $str = fgets($fd);
+                $str = preg_replace_callback('/\%\%(.*?)\%\%/si', function($data) use ($av) {
+                    return ($av['param'][$data[1]]);
+                }, $str);
+                $this->_html .= $str;
+            }
+            fclose($fd);
+        }
+        function __destruct() {
+            
+        }
+        public function getHtml() {
+            return $this->_html;
+        }
+    }
+?>
